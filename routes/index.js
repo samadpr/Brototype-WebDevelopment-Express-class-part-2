fd7efcb2f4d1955bcaa0520b85b37a25e24605fb
Abdul Samad <codinguse341@gmail.com>
2022-07-20 15:07:33 +0530

    same changes in index.js and inex.hbs

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res, next) {
 
   const values=['samad', 'shehin', 'afthab', 'sibin']
 
-  const person={name:"samad", comments:{comment:"This is a sample comment", date:"04-08-2022"}}
+  const person={name:"samad", admin:false}
 
   res.render('index', {person});
-  //res.render('index', { title: 'My Website' ,Name:'samadpr'});
+  
 });
 
 module.exports = router;
